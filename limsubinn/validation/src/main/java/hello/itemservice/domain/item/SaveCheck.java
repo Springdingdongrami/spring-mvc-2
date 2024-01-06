88c091774bcb59826ab8d309b3e0ff5565854316
limsubinn <mymyx02@naver.com>
2024-01-06 20:09:52 +0900

    5. 검증2 - Bean Validation
    Bean Validation - groups

---
+++ limsubinn/validation/src/main/java/hello/itemservice/domain/item/SaveCheck.java
@@ -0,0 +1,6 @@
+package hello.itemservice.domain.item;
+
+// 저장용 그룹 표시
+public interface SaveCheck {
+}
+
---
+++ limsubinn/validation/src/main/java/hello/itemservice/domain/item/UpdateCheck.java
@@ -0,0 +1,6 @@
+package hello.itemservice.domain.item;
+
+// 수정용 그룹 표시
+public interface UpdateCheck {
+}
+
