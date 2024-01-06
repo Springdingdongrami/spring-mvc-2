36dabdfb366841f02b0ae8a94d03abe10b9a5c99
limsubinn <mymyx02@naver.com>
2024-01-07 01:04:18 +0900

    6. 로그인 처리1 - 쿠키, 세션
    로그인 처리하기 - 직접 만든 세션 적용

--- limsubinn/login/src/main/java/hello/login/web/HomeController.java
+++ limsubinn/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.web.member.Member;
 import hello.login.web.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,14 +17,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+//    @GetMapping("/")
+    public String homeLoginV1(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         // 로그인 X
         if (memberId == null) {
             return "home";
@@ -36,4 +39,20 @@ public class HomeController {
         model.addAttribute("member", loginMember); // 로그인 정보 유효할 경우
         return "loginHome";
     }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+        // 로그인 X
+        if (member == null) {
+            return "home";
+        }
+
+        // 로그인 O
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
 }
--- limsubinn/login/src/main/java/hello/login/web/login/LoginController.java
+++ limsubinn/login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,7 +2,9 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.web.member.Member;
+import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +21,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form,
+//    @PostMapping("/login")
+    public String loginV1(@Valid @ModelAttribute LoginForm form,
                         BindingResult bindingResult,
                         HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
@@ -47,9 +50,36 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
+    @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm form,
+                          BindingResult bindingResult,
+                          HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        // 로그인 실패 처리
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리 -> 세션 관리자를 통해 세션을 생성하고 회원 데이터를 보관
+        sessionManager.createSession(loginMember, response);
+        return "redirect:/";
+    }
+
+//    @PostMapping("/logout")
+    public String logoutV1(HttpServletResponse response) {
         expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
         return "redirect:/";
     }
 
