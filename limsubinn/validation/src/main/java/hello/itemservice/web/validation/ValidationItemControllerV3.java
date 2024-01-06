2ab9a624dc6cfdd314b07b1d664254883d1c3494
limsubinn <mymyx02@naver.com>
2024-01-06 19:40:13 +0900

    5. 검증2 - Bean Validation
    Bean Validation - 수정에 적용

--- limsubinn/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ limsubinn/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -46,12 +46,13 @@ public class ValidationItemControllerV3 {
     public String addItem(@Validated @ModelAttribute Item item,
                             BindingResult bindingResult,
                             RedirectAttributes redirectAttributes) {
-        //특정 필드 예외가 아닌 전체 예외
+        // 특정 필드 예외가 아닌 전체 예외
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            } }
+            }
+        }
 
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
@@ -74,7 +75,20 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @ModelAttribute @Validated Item item, BindingResult bindingResult) {
+        // 특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
